093d64d240e49fdb69af077acd7d84671e5361b9
Santiago Gonda <santiago.j.g.t@gmail.com>
2023-09-14 21:00:03 -0300

    ajuste

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from "./App";
 
-//import {msj} from "./App";
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
-
-//console.log(msj);
 
 root.render(<App/>);
